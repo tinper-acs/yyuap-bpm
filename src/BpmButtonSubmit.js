b49d3e1c3703cf636f02409ef0344b616d4b0717
kvkens <kvkens@qq.com>
2018-07-19 16:57:37 +0800

    add:提交流程添加指派.

--- src/BpmButtonSubmit.js
+++ src/BpmButtonSubmit.js
@@ -2,8 +2,9 @@
  * bpm 提交流程按钮
  */
 import React, { Component } from 'react';
-import { Row, Col, Button, Message } from 'tinper-bee';
-import { queryBpmTemplateAllocate, onCommit, reconvert } from './common';
+import axios from 'axios';
+import { Radio, Row, Col, FormControl, Button, Modal, Message, Table, Checkbox, Pagination } from 'tinper-bee';
+import { queryBpmTemplateAllocate, onCommit, reconvert, sendBpmTaskAJAX } from './common';
 import PropTypes from 'prop-types';
 const propTypes = {
     checkedArray: PropTypes.array,
@@ -11,7 +12,7 @@ const propTypes = {
     funccode: PropTypes.string,
     nodekey: PropTypes.string,
     url: PropTypes.string,
-    data: PropTypes.array,
+    urlAssignSubmit: PropTypes.string,
     className: PropTypes.string,
     onSuccess: PropTypes.func,
     onError: PropTypes.func,
@@ -21,30 +22,75 @@ const propTypes = {
 class BpmButtonSubmit extends Component {
     constructor() {
         super();
+        this.state = {
+            name: "",
+            checkedArray: [],
+            signAddList: [],
+            signAddShow: false,
+            pageNum: 1,
+            pageSize: 20,
+            totalPages: 0,
+            userIds: [],
+            processDefineCode: "",
+            assignInfo: {
+                assignInfoItems: []
+            },
+            obj: {},//单据数据
+            huanjieShow: false,//环节指派显示
+            editRowIndex: 0,
+
+        }
+        //驳回到环节的Modal-Table
+        this.signAddCol = [{
+            title: "名称",
+            dataIndex: "name",
+            key: "name",
+            width: "30%"
+        },
+        {
+            title: "编码",
+            dataIndex: "code",
+            key: "code",
+            width: "30%"
+        }]
+        //环节Col
+
     }
+    //提交流程按钮
     handlerBtn = async () => {
-        let errFlag = false;
-        let { checkedArray, data, onStart, onSuccess, onError } = this.props;
+        // let errFlag = false;
+        let { checkedArray, onStart, onSuccess, onError } = this.props;
+        //加载事件
         if (onStart) {
             onStart();
         }
+        //筛选选择单据
         let submitArray = [];
-        for (let i = 0; i < checkedArray.length; i++) {
-            if (checkedArray[i].bpmState == null || checkedArray[i].bpmState == 0) {
-                submitArray.push({ "id": checkedArray[i].id });
-                errFlag = false;
-            } else {
-                errFlag = true;
-                onError && onError({
-                    type: 1,
-                    msg: `单据 ${checkedArray[i].id} 不能重复提交`
-                });
-            }
-        }
-        if (errFlag) {
+        // for (let i = 0; i < checkedArray.length; i++) {
+        //     if (checkedArray[i].bpmState == null || checkedArray[i].bpmState == 0) {
+        //         submitArray.push({ "id": checkedArray[i].id });
+        //         errFlag = false;
+        //     } else {
+        //         errFlag = true;
+        //         onError && onError({
+        //             type: 1,
+        //             msg: `单据 ${checkedArray[i].id} 不能重复提交`
+        //         });
+        //     }
+        // }
+        // if (errFlag) {
+        //     return;
+        // }
+        //检查只能一条单据提交流程
+        if (checkedArray.length >= 2) {
+            onError && onError({
+                type: 2,
+                msg: `请选择单条数据提交`
+            });
             return;
         }
-        if (submitArray.length > 0) {
+        //处理数据提交第一次请求，然后发起二次请求
+        if (checkedArray.length > 0) {
             let { data: { success, detailMsg } } = await queryBpmTemplateAllocate({
                 funccode: this.props.funccode,
                 nodekey: this.props.nodekey
@@ -52,20 +98,38 @@ class BpmButtonSubmit extends Component {
             if (success == "success") {
                 let commitParam = {
                     "url": this.props.url,
-                    "processDefineCode": detailMsg["data"]["res_code"],
-                    "submitArray": submitArray
+                    "processDefineCode": detailMsg.data.res_code,
+                    "submitArray": checkedArray
                 }
-                //commit提交后，返回success状态显示fail_global
+                this.setState({
+                    processDefineCode: detailMsg.data.res_code
+                });
+                //收集参数准备提交submit
                 let result = await onCommit(commitParam);
-                let flag = result["data"]["success"];
-                if (flag == "success") {
+                let flag = result.data.success;
+                if (flag == "success" && (typeof result.data.assignAble == 'undefined')) {
                     //正确
                     onSuccess && onSuccess();
-                } else {
+                } else if (flag == "fail_global") {
                     onError && onError({
                         type: 2,
                         msg: `流程启动失败`
                     });
+                }
+
+                if (result.data.assignAble == 'true') {
+                    //判断是否有最新的活动id和name
+                    if (result.data.assignedActivities && result.data.assignedActivities.length > 0) {
+                        //更新环节指派数据
+                        this.setState({
+                            huanjieShow: true,
+                            huanjieList: result.data.assignedActivities,
+                            obj: checkedArray[0],
+                            assignInfo: {
+                                assignInfoItems: Array.from(result.data.assignedActivities, x => ({ activityId: x.id, activityName: x.name, participants: [] }))
+                            }
+                        });
+                    }
                 }
             } else if (success == "fail_global") {
                 let { data: { message } } = result
@@ -84,11 +148,299 @@ class BpmButtonSubmit extends Component {
         }
 
     }
+    onAllCheckChange = () => {
+        let self = this;
+        let checkedArray = self.state.checkedArray;
+
+        let listData = self.state.signAddList.concat();
+        let selIds = [];
+        // let id = self.props.multiSelect.param;
+        for (var i = 0; i < self.state.checkedArray.length; i++) {
+            checkedArray[i].isChecked = !self.state.checkedAll;
+        }
+        // if (self.state.checkedAll) {
+        //   selIds = [];
+        // } else {
+        //   for (var i = 0; i < listData.length; i++) {
+        //     selIds[i] = listData[i][id];
+        //   }
+        // }
+        self.setState({
+            checkedAll: !self.state.checkedAll,
+            checkedArray: checkedArray,
+            // selIds: selIds
+        });
+        // self.props.onSelIds(selIds);
+        let userIdArr = checkedArray.filter((item) => item.isChecked);
+        let userIdsArray = [];
+        userIdArr.map((item) => {
+            userIdsArray.push(item.id);
+        });
+        this.setState({
+            userIds: userIdsArray
+        });
+    };
+    //选择多选
+    onCheckboxChange = (text, record, index) => {
+        let self = this;
+        let allFlag = false;
+        // let selIds = self.state.selIds;
+        // let id = self.props.postId;
+        let checkedArray = self.state.checkedArray.concat();
+        // if (self.state.checkedArray[index]) {
+        // selIds.remove(record[id]);
+        // } else {
+        // selIds.push(record[id]);
+        // }
+        checkedArray[index].isChecked = !self.state.checkedArray[index].isChecked;
+        checkedArray[index].id = record.id;
+        for (var i = 0; i < self.state.checkedArray.length; i++) {
+            if (!checkedArray[i].isChecked) {
+                allFlag = false;
+                break;
+            } else {
+                allFlag = true;
+            }
+        }
+        self.setState({
+            checkedAll: allFlag,
+            checkedArray: checkedArray,
+            // selIds: selIds
+        });
+        // self.props.onSelIds(selIds);
+        let userIdArr = checkedArray.filter((item) => item.isChecked);
+        let userIdsArray = [];
+        userIdArr.map((item) => {
+            userIdsArray.push(item.id);
+        });
+        this.setState({
+            userIds: userIdsArray
+        });
+    }
+    //使用多选框
+    renderColumnsMultiSelect(columns) {
+        const { checkedArray } = this.state;
+        const { multiSelect } = this.props;
+        let select_column = {};
+        let indeterminate_bool = false;
+        // let indeterminate_bool1 = true;
+        if (multiSelect && multiSelect.type === "checkbox") {
+            let i = checkedArray.length;
+            while (i--) {
+                if (checkedArray[i].isChecked) {
+                    indeterminate_bool = true;
+                    break;
+                }
+            }
+            let defaultColumns = [
+                {
+                    title: (
+                        <Checkbox
+                            className="table-checkbox"
+                            checked={this.state.checkedAll}
+                            indeterminate={indeterminate_bool && !this.state.checkedAll}
+                            onChange={this.onAllCheckChange}
+                        />
+                    ),
+                    key: "checkbox",
+                    dataIndex: "checkbox",
+                    width: "5%",
+                    render: (text, record, index) => {
+                        return (
+                            <Checkbox
+                                className="table-checkbox"
+                                checked={this.state.checkedArray[index].isChecked}
+                                onChange={this.onCheckboxChange.bind(this, text, record, index)}
+                            />
+                        );
+                    }
+                }
+            ];
+            columns = defaultColumns.concat(columns);
+        }
+        return columns;
+    }
+    //搜索加签人员name
+    handlerSignAddSearchValue = (value) => {
+        this.setState({
+            name: value
+        })
+    }
+    //查找加签人员
+    handlerSignAddSearch = async () => {
+        let result = await sendBpmTaskAJAX('signAdd', this.state);
+        this.setState({
+            signAddList: result.data.data.content,
+            checkedArray: result.data.data.content,
+            totalPages: result.data.data.totalPages
+        });
+    }
+    //分页条查询
+    handlerSignAddPage = (page) => {
+        this.setState({
+            pageNum: page
+        }, async () => {
+            let result = await sendBpmTaskAJAX('signAdd', this.state);
+            this.setState({
+                signAddList: result.data.data.content,
+                checkedArray: result.data.data.content,
+                totalPages: result.data.data.totalPages
+            });
+        });
+    }
+    //通用关闭方法
+    close = () => {
+        this.setState({
+            signAddShow: false,
+            selectedRow: [],
+            signAddList: [],
+            checkedArray: [],
+            checkedAll: false,
+            name: ""
+        });
+    }
+    closeHuanjie = () => {
+        this.setState({
+            huanjieShow: false
+        });
+    }
+    //选择人员后的确定事件
+    signAddOK = () => {
+        let _index = this.state.editRowIndex;//修改第几个数据
+        let sourseArray = this.state.assignInfo.assignInfoItems.slice();
+        sourseArray[_index]['participants'] = this.state.userIds;
+        this.setState({
+            assignInfo: {
+                assignInfoItems: sourseArray
+            },
+            userIds: [],
+            signAddShow: false
+        });
+    }
+    //选择完所有加签后的确定事件
+    huanjieHandlerOK = async () => {
+        let { onSuccess, onError } = this.props;
+        let { processDefineCode, assignInfo, obj } = this.state;
+        let result = await axios.post(this.props.urlAssignSubmit, {
+            processDefineCode,
+            assignInfo,
+            obj
+        }).catch((e) => {
+            onError && onError({
+                type: 2,
+                msg: `后台服务请求发生错误`
+            });
+        });
+        if (result.data.success == 'success') {
+            onSuccess && onSuccess();
+            this.setState({
+                huanjieShow: false
+            });
+        } else if (result.data.success == 'fail_global') {
+            onError && onError({
+                type: 2,
+                msg: `提交发生了错误`
+            });
+        }
+
+    }
     render() {
         let { text } = this.props;
-        return (
+        let self = this;
+        let huanjieCol = [{
+            title: "名称",
+            dataIndex: "name",
+            key: "name",
+            width: "30%"
+        },
+        {
+            title: "编码",
+            dataIndex: "id",
+            key: "id",
+            width: "30%"
+        }, {
+            title: "指派",
+            dataIndex: "1",
+            key: "1",
+            width: "30%",
+            render(text, record, index) {
+                return <Button colors="primary" size="sm" onClick={() => {
+                    //提前加载指派人员数据
+                    self.handlerSignAddSearch();
+                    self.setState({
+                        editRowIndex: index,
+                        signAddShow: true
+                    });
+                }}>选择</Button>
+            }
+        }]
+        return (<span>
             <Button className={this.props.className} size="sm" onClick={this.handlerBtn} colors="primary">{text}</Button>
-        );
+            <Modal
+                show={this.state.signAddShow}
+                backdrop={false}
+                onHide={this.close}>
+                <Modal.Header closeButton>
+                    <Modal.Title> 加签人员列表 </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Row style={{ "paddingBottom": "10px" }}>
+                        <Col md={2}>
+                            <div style={{ "lineHeight": "30px" }}>名称：</div>
+                        </Col>
+                        <Col md={5}>
+                            <FormControl
+                                value={this.state.name}
+                                onChange={this.handlerSignAddSearchValue}
+                            />
+                        </Col>
+                        <Col md={2}>
+                            <Button
+                                style={{ "marginLeft": "10px" }}
+                                onClick={this.handlerSignAddSearch}
+                                colors="primary">查询</Button>
+                        </Col>
+                    </Row>
+                    <Table
+                        scroll={{ y: 200 }}
+                        rowKey={record => record.code}
+                        columns={this.renderColumnsMultiSelect(this.signAddCol)}
+                        data={this.state.signAddList}
+                    />
+                    <Pagination
+                        boundaryLinks
+                        prev
+                        next
+                        items={this.state.totalPages}
+                        activePage={this.state.pageNum}
+                        onSelect={this.handlerSignAddPage} />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button colors="primary" style={{ "marginRight": "10px" }} onClick={this.signAddOK}> 确定 </Button>
+                    <Button colors="primary" onClick={this.close}> 关闭 </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal
+                show={this.state.huanjieShow}
+                backdrop={false}
+                onHide={this.closeHuanjie}>
+                <Modal.Header closeButton>
+                    <Modal.Title> 环节指派 </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Table
+                        rowKey={record => record.id}
+                        columns={huanjieCol}
+                        data={this.state.huanjieList}
+                    />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button colors="primary" style={{ "marginRight": "10px" }} onClick={this.huanjieHandlerOK}> 确定 </Button>
+                    <Button colors="primary" onClick={this.closeHuanjie}> 关闭 </Button>
+                </Modal.Footer>
+            </Modal>
+        </span>);
     }
 }
 BpmButtonSubmit.propTypes = propTypes;
@@ -97,7 +449,11 @@ BpmButtonSubmit.defaultProps = {
     text: "提交",
     nodekey: "003",
     url: "/example/ygdemo_yw_info/submit",
-    data: [],
-    className: ""
+    urlAssignSubmit: "/example/ygdemo_yw_info/assignSubmit",
+    className: "",
+    multiSelect: {
+        type: "checkbox",
+        param: "key"
+    }
 }
 export default BpmButtonSubmit;
--- src/demo.js
+++ src/demo.js
@@ -28,50 +28,22 @@ class Demo extends Component {
                     onSuccess={() => console.log('success')}
                     onError={() => console.log('error')}
                     appType="1"
-                    id="ca15d4cf-74f9-4f57-af4e-c6803eab2d87"
+                    id="ea69b9bf-d8b5-47a7-a1fd-1114f36a9321"
                 />
                 <BpmFlowChart />
                 <BpmTable />
                 <BpmButtonSubmit
-                    funccode="react"
+                    funccode="ygdemo_yw_info"
                     nodekey="003"
-                    url={`/iuap_pap_quickstart/example_workorder/submit`}
+                    url={`/ygdemo_yw_info/submit`}
+                    urlAssignSubmit={`/ygdemo_yw_info/assignSubmit`}
                     checkedArray={
                         [
-                            {
-                                "id": "eb2c8a6bf7c449548740a70e2187da55",
-                                "createTime": null,
-                                "createUser": null,
-                                "lastModified": "2018-07-04 12:35:30 610",
-                                "lastModifyUser": "9943304b46df4a85a4e43c95653159d2",
-                                "ts": "2018-07-04 08:00:46 163",
-                                "dr": 0,
-                                "bpmState": 1,
-                                "taskKey": null,
-                                "taskId": null,
-                                "processInstanceId": null,
-                                "processDefineCode": null,
-                                "comment": null,
-                                "orderCode": "201807041600464",
-                                "orderName": null,
-                                "supplier": null,
-                                "supplierName": "16供应商名称",
-                                "type": "0",
-                                "purchasing": "16采购组织",
-                                "purchasingGroup": "14采购组",
-                                "voucherDate": 1530633600000,
-                                "approvalState": 1,
-                                "confirmState": 0,
-                                "closeState": 0,
-                                "type_name": "投诉工单",
-                                "approvalState_name": "已提交",
-                                "confirmState_name": "未确认",
-                                "closeState_name": "未关闭",
-                                "remark": null,
-                                "bpmBillCode": null
-                            }
+                            { "id": "ea69b9bf-d8b5-47a7-a1fd-1114f36a9321", "code": "180719001", "name": "岳明-01", "ly_code": "1", "ly_sm": "岳明-01\u0000", "zr_dw": "97da3229-3308-4492-a457-cb4d4e6264a4", "zr_dw_name": "用友股份", "zrr": null, "xb_dw": null, "xb_dw_name": null, "xbr": null, "begin_date": null, "end_date": null, "zy_cd": 2, "qt_ld": null, "zbr": null, "zbr_name": null, "dbr": null, "jfyq": null, "db_info": null, "jd_bl": null, "rwpf": null, "kpi_flag": 0, "kpi_level": 1, "state": 0, "create_name": null, "create_name_name": null, "create_time": null, "update_name": null, "update_name_name": null, "update_time": null, "unitid": null, "unitid_name": null, "id_ygdemo_yw_sub": null, "metaDefinedName": "ygdemo_yw_info", "namespace": "iuap_qy", "status": 0, "changedPropertyNames": null, "tenant_id": "tenant", "dr": 0, "ts": 1531970736000 }
                         ]
                     }
+                    onSuccess={() => console.log('success')}
+                    onError={(err) => console.log(err)}
                 />
                 <BpmButtonRecall
                     url={`/iuap_pap_quickstart/example_workorder/recall`}
