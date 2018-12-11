aabaf7eb07bc570bae0fddc8500094bc11029ef5
zhangjlt <zhangjlt@yonyou.com>
2018-12-11 10:58:10 +0800

    修改 流程列表 超时时间

--- src/BpmTable.js
+++ src/BpmTable.js
@@ -70,8 +70,8 @@ class BpmTable extends Component {
             },
             {
                 title: "超时时间",
-                dataIndex: "claimTime",
-                key: "claimTime"
+                dataIndex: "dueDate",
+                key: "dueDate"
             }
         ];
     }
